fc9a243f9096ecab7df7029232080a7da374dfc6
Francy Lang <francy.lang@gmail.com>
2017-10-12 12:59:37 -0600

    hurry push

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../../index.css';
 import CardContainer from '../CardContainer/CardContainer.js';
 import ButtonContainer from '../ButtonContainer/ButtonContainer.js';
-//import Video from '../Video/Video.js';
+import Video from '../Video/Video.js';
 import Scroll from '../Scroll/Scroll.js';
 import './App.css';
 import {
@@ -101,27 +101,26 @@ class App extends Component {
       );
     }  else {
       return (
-      <main className="App">
-        {console.log('we are inside ELSE')}
-       <h1 className="main-title">SWAPI<span className="main-title-two">BOX</span></h1>
-       <ButtonContainer
-         handleClick={this.handleClick}
-         selected={this.state.selected} />
-        <section>
-         <article className='cards'>
-         <CardContainer
-           vehicles={this.state.cleanedVehicles}
-           planets={this.state.cleanedPlanets}
-           people={this.state.cleanedPeople}
-           selected={this.state.selected} />
-         </article>
-         <article className='video-container'>
-           <Video />
-           {
-             this.state.scroll.length && <Scroll scrollData={this.state.scroll} />
-           }
-         </article>
-       </section>
+        <main className="App">
+          <h1 className="main-title">SWAPI<span className="main-title-two">BOX</span></h1>
+          <ButtonContainer
+            handleClick={this.handleClick}
+            selected={this.state.selected} />
+          <section>
+            <article className='cards'>
+              <CardContainer
+                vehicles={this.state.cleanedVehicles}
+                planets={this.state.cleanedPlanets}
+                people={this.state.cleanedPeople}
+                selected={this.state.selected} />
+            </article>
+            <article className='video-container'>
+              <Video />
+              {
+                this.state.scroll.length && <Scroll scrollData={this.state.scroll} />
+              }
+            </article>
+          </section>
         </main>
       );
     }
